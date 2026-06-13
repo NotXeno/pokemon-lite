da546117c5620d5421b815e680863d7a1f3177a3
NotXeno <alvino.wilbert.ho@gmail.com>
2026-06-13 13:55:50 +0700

    V2.1.1 : Upgrading the move effects

--- frontend/src/components/Battle.jsx
+++ frontend/src/components/Battle.jsx
@@ -100,11 +100,11 @@ export default function Battle({
                     setLeftAnimClass(isHeal ? 'animate-heal' : 'animate-attack-left');
                     setTimeout(() => setLeftAnimClass(''), 300);
                     if (isHeal) {
-                        setLeftEffect(effectImage);
+                        setLeftEffect({ image: effectImage, type: 'heal' });
                         setTimeout(() => setLeftEffect(null), 800);
                     } else {
                         setTimeout(() => {
-                            setRightEffect(effectImage);
+                            setRightEffect({ image: effectImage, type: 'attack' });
                             setTimeout(() => setRightEffect(null), 800);
                         }, 200);
                     }
@@ -112,11 +112,11 @@ export default function Battle({
                     setRightAnimClass(isHeal ? 'anim-heal' : 'animate-attack-right');
                     setTimeout(() => setRightAnimClass(''), 300);
                     if (isHeal) {
-                        setRightEffect(effectImage);
+                        setRightEffect({ image: effectImage, type: 'heal' });
                         setTimeout(() => setRightEffect(null), 800);
                     } else {
                         setTimeout(() => {
-                            setLeftEffect(effectImage);
+                            setLeftEffect({ image: effectImage, type: 'attack' });
                             setTimeout(() => setLeftEffect(null), 800);
                         }, 200);
                     }
@@ -187,7 +187,11 @@ export default function Battle({
                     <div className="relative w-28 h-28 md:w-40 lg:w-48 mr-4 flex-shrink-0 mt-4 z-10">
                         <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-20 h-4 bg-black/20 rounded-[100%]"></div>
                         <img src={`/pokemon-images/${getPokeImg(rightActivePoke?.name, (rightActivePoke?.hp || 0) > 0 ? 'active' : 'fainted', 'front')}`} className={`absolute bottom-4 left-1/2 -translate-x-1/2 w-2/3 md:w-3/4 object-contain ${(rightActivePoke?.hp || 0) > 0 ? 'animate-bounce' : 'translate-y-8 grayscale sepia'} ${rightAnimClass}`} />
-                        {rightEffect && <img src={`/Effects/${rightEffect}`} className="effect-image" />}
+                        {rightEffect && (
+                            <div className={`absolute top-1/2 left-1/2 w-full h-full z-[60] pointer-events-none ${rightEffect.type === 'heal' ? 'effect-heal-anim' : 'effect-fly-from-player'}`}>
+                                <img src={`/Effects/${rightEffect.image}`} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[250%] h-[250%] max-w-none object-contain" />
+                            </div>
+                        )}
                     </div>
                 </div>
 
@@ -195,7 +199,7 @@ export default function Battle({
                 <div className="flex justify-between items-end w-full relative pb-2 mt-2">
                     <div className="relative w-32 h-32 md:w-44 lg:w-52 ml-4 flex-shrink-0 -mb-2 z-50 mt-4">
                         <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-28 h-5 bg-black/30 rounded-[100%]"></div>
-                        <img src={`/pokemon-images/${getPokeImg(leftActivePoke?.name, (leftActivePoke?.hp || 0) > 0 ? 'active' : 'fainted', 'back')}`} className={`absolute -bottom-2 left-1/2 -translate-x-1/2 w-3/4 md:w-[90%] object-contain drop-shadow-2xl ${(leftActivePoke?.hp || 0) > 0 ? '' : 'translate-y-8 grayscale sepia'} ${leftAnimClass}`} onError={(e) => { e.target.style.display = 'none'; }} />
+                        <img src={`/pokemon-images/${getPokeImg(leftActivePoke?.name, (leftActivePoke?.hp || 0) > 0 ? 'active' : 'fainted', 'back')}`} className={`absolute -bottom-2 left-1/2 -translate-x-1/2 w-3/4 md:w-[90%] object-contain drop-shadow-2xl ${(leftActivePoke?.hp || 0) > 0 ? '' : 'translate-y-8 grayscale sepia'} ${leftAnimClass}`} />
                         {leftEffect && <img src={`/Effects/${leftEffect}`} className="effect-image" />}
                     </div>
                     <div className="bg-white border-4 border-gray-900 p-2 rounded-xl shadow-[4px_4px_0px_rgba(0,0,0,0.5)] w-40 md:w-56 lg:w-64 relative z-30 self-end">
